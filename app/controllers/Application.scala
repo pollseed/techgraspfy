0c7812d5592a27ecd35fd247bdd8f2bfbb71c5c7
pollseed <cb.so.devope+0001@gmail.com>
2014-11-27 09:27:34 +0900

    test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,8 +6,31 @@ import akka.actor.Actor
 import akka.actor.Props
 import scala.collection.immutable.Map
 import play.api.libs.iteratee.Enumerator
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
+
+  case class UserData(name: String, age: Int)
+
+  val userForm = Form(
+    mapping(
+      "name" -> text,
+      "age" -> number)(UserData.apply)(UserData.unapply)
+      verifying ("Failed form constraints", fields => fields match {
+        case userData => validate(userData.name, userData.age).isDefined
+      }))
+
+  def validate(name: String, age: Int) = {
+    name match {
+      case "bob" if age >= 18 =>
+        Some(UserData(name, age))
+      case "admin" =>
+        Some(UserData(name, age))
+      case _ =>
+        None
+    }
+  }
 
   val SESSION_KEY = "SESSION_KEY"
 
