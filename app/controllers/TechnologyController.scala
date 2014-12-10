46aac55ee4f41c5b38e627ebb96495be51d9b7a0
pollseed <cb.so.devope+0001@gmail.com>
2014-12-11 01:27:05 +0900

    Update TechnologyController.scala

--- app/controllers/TechnologyController.scala
+++ app/controllers/TechnologyController.scala
@@ -5,6 +5,8 @@ import play.api.mvc._
 import play.api.data._
 
 object TechnologyController extends Controller {
+  
+  def index = Action { Redirect(routes.DashboardController technology) }
 
   /**
    * プログラミング言語を登録します
